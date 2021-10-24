8cc1c0241af15287f622bd6114ad871733a09b35
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2021-10-24 16:36:02 -0600

    Major Changes:   * Code has been converted to D3 version 7 compatibility   * Pan and Zoom are now functional
    Minor Changes:
      * None

--- Polis/main_viz.js
+++ Polis/main_viz.js
@@ -1,11 +1,17 @@
 var margin = { top: 75, left: 75, right: 75, bottom: 75},
-    height = 800 - margin.top - margin.bottom,
-    width = 1400 - margin.left - margin.right;
+     height = 800 - margin.top - margin.bottom,
+     width = 1400 - margin.left - margin.right;
+
+// Whenever there are shapes on a map we want to use 'geoMercator' (generally) and 'geoPath' apparently
+var projection = d3.geoMercator()
+    .center([30, 40 ]) // Roughly puts the mediterranean area we need in the center
+    .translate([width/2, height/2])
+    .scale(1900); // Essentially creates the level of zoom on our map we'll see
 
 var svgMap = d3.select("#map")
     .append("svg")
-    .attr("height", height + margin.top + margin.bottom)
     .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .attr("border", 2)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -17,110 +23,58 @@ var borderPath = svgMap.append("rect")
     .attr("height", height + margin.top + margin.bottom)
     .attr("width", width + margin.left + margin.right)
     .style("stroke", "black")
-    .style("fill", "#AED6F1") // Essentially gives our oceans color.
+    .style("fill", "#AED6F1") // Essentially gives our oceans color
     .style("stroke-width", 4);
 
-// The following does not work.
-/*Promise.all([
-    d3.json("data/countries-10m.json"), d3.csv("data/polis data_distributed 21.08.25.csv")
-]).then(ready)*/
-
-d3.queue()
-    .defer(d3.json, "data/countries-10m.json") // first item here so second in 'function ready()'
-    .defer(d3.csv, "data/polis data_distributed 21.08.25.csv") // second item here so third in 'function ready()'
-    .await(ready)
-
-// Whenever there are shapes on a map we want to use 'geoMercator' (generally) and 'geoPath' apparently
-var projection = d3.geoMercator()
-    .center([ 29, 41 ]) // Roughly puts the mediterranean area we need in the center
-    .translate([width/2, height/2])
-    .scale(1900) // essentially creates the level of zoom on our map we'll see
-
 var mapPath = d3.geoPath()
-    .projection(projection)
+    .projection(projection);
 
-// Adapted from "https://bl.ocks.org/vasturiano/f821fc73f08508a3beeb7014b2e4d50f" but does not work presently
-/*var zoom = d3.zoom()
-    .scaleExtent([1, 8])
-    .on('zoom', zoomed)
+var g = svgMap.append("g");
 
-svgMap.call(zoom)
+// The following will display the Mediterranean part of the world
+d3.json("data/world-50m.json").then(function(worldTopo) {
 
-d3.json('data/countries-10m.json')
-    .then(world => {
-        g.append('path')
-            .datum({ type: 'Sphere' })
-            .attr('class', 'sphere')
-            .attr('d', mapPath)
-
-        g.append('path')
-            .datum(topojson.merge(world, world.objects.countries.geometries))
-            .attr('class', 'land')
-            .attr('d', mapPath)
-
-        g.append('path')
-            .datum(topojson.mesh(world, world.objects.countries, (a, b) => a !== b))
-            .attr('class', 'boundary')
-            .attr('d', mapPath)
-    })
-
-function zoomed() {
-    svgMap
-        .selectAll('mapPath')
-        .attr('transform', d3.event.transform)
-}
-*/
-
-function ready (error, data, city) {
-    //console.log(city)
-
-    var text_color = "#74FF00"
-    var countries = topojson.feature(data, data.objects.countries).features
-
-    svgMap.selectAll(".country")//.state")
-        .data(countries)
+    g.selectAll("countries")
+        .data(topojson.feature(worldTopo, worldTopo.objects.countries)
+            .features)
         .enter().append("path")
-        .attr("class", "country")
         .attr("d", mapPath) // grabs the 'map_path' variable i made and filled with 'geoPath' and then displays our map in the browser
-        //.attr("title", "country name") // This won't work. We need to do something different with SVG.
         .attr("fill", "none") // no color so that there is no interference with my '.county' layer
         .attr("stroke", "#186A3B")
-        .attr("stroke-width", "1.9")
-//            .append("title")
-//            .text("country name");
+        .attr("stroke-width", "1.9");
 
+    // load and display the cities
+    d3.csv("data/polis data_distributed 21.08.25.csv").then(function(polisData) {
+        g.selectAll("city-marks")
+            .data(polisData)
+            .enter()
+            .append("a")
+            .append("circle")
 
-    // Zooming is not working, though the console does show a 'zoom' variable to go way up as I use mousewheel.
-/*    svgMap.call(d3.zoom().on("zoom", () => {
-        console.log("zoom");
-    }))
-*/
-    svgMap.selectAll(".city-marks")
-        .data(city)
-        .enter().append("circle")
-        .attr("r", 1.65)
-        .attr("fill", "#F44336")//"#34495E")
+            // the lat and long must be converted to x and y coordinates (as was discussed in lecture -- turns out this is true)
+            .attr("cx", function(d) {
+                return projection([d.Longitude, d.Latitude])[0]; // Returns only x coord
+            })
+            .attr("cy", function(d) {
+                return projection([d.Longitude, d.Latitude])[1]; // returns only y coord
+            })
+            .attr("r", 1.65)
+            .style("fill", "#F44336")
+            .attr("opacity", "0.85") // makes each dot slightly opaque
+            .append("title")
+            .text((d) => d.Name); // Pulls city name of corresponding coordinates. JS Note: () => {} is a fast way of creating a function and the part after the arrows is the return.
+    });
+});
 
-        // the lat and long must be converted to x and y coordinates (as was discussed in lecture -- turns out this is true)
-        .attr("cx", function (d) {
-            // notice we must feed in both 'long' and 'lat' to get 'x' coord
-            var coords = projection([d.Longitude, d.Latitude])
-            return coords[0]; // returns 'x' only
-        })
-        .attr("cy", function (d) {
-            var coords = projection([d.Longitude, d.Latitude])
-            return coords[1]; // returns 'y' only
-        })
+var zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on('zoom', function(event) {
+        g.selectAll('path')
+            .attr('transform', event.transform);
+        g.selectAll("circle")
+            .attr('transform', event.transform);
+        g.selectAll("text")
+            .attr('transform', event.transform);
+    });
 
-        .attr("opacity", "0.85") // makes each dot slightly opaque
-
-        .append("title")
-
-        // Pulls city name of corresponding coordinates. JS Note: () => {} is a fast way of creating a function and the part after the arrows is the return.
-        .text((d) => d.Name)
-
-    svgMap.selectAll(".city-name")
-        .data(city)
-        .enter().append("text")
-        .attr("class", "city-name")
-}
+svgMap.call(zoom);
