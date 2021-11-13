dcc127781bd2fbea01107490678d858f4b740a0d
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2021-11-12 17:28:06 -0700

    Major Changes:   * The network project will likely consist of nodes and links. As of right now that is my direction with it.   * We have added nodes, links and names on nodes.   * The temporary data consists of programming languages.   * The temp data is contained in 'main_viz.js' for now.
    Minor Changes:
      * None

---
+++ Network/main_viz.js
@@ -0,0 +1,87 @@
+(function (d3)
+{
+    'use strict';
+
+    const svg = d3.select('#networkCont');
+
+    const width = +svg.attr('width');
+    const height = +svg.attr('height');
+    const centX = width/2;
+    const centY = height/2;
+
+    const testNodes = [
+        {"id": "Java"},
+        {"id": "Python"},
+        {"id": "COBOL"},
+        {"id": "R"},
+        {"id": "Charm"},
+        {"id": "C++"},
+        {"id": "Swift"},
+        {"id": "F#"},
+        {"id": "Alice"}
+    ]
+
+    const testLinks = [
+        {"source": 0, "target": 1}, // Java -> Python
+        {"source": 1, "target": 2},
+        {"source": 2, "target": 2},
+        {"source": 2, "target": 3},
+        {"source": 2, "target": 4},
+        {"source": 3, "target": 4},
+        {"source": 4, "target": 5},
+        {"source": 4, "target": 6},
+        {"source": 6, "target": 7},
+        {"source": 6, "target": 8}
+    ];
+
+    const testSim = d3.forceSimulation(testNodes)
+        .force("charge", d3.forceManyBody())
+        .force("link", d3.forceLink(testLinks))
+        .force("center", d3.forceCenter(centX, centY));
+
+
+    // For the D3 'scaleOrdinal()' function it doesn't matter what the data is... it's just counting the number of elements.
+    const circleData = [1,2,3,4,5,6,7,8,9,10]
+    const lineData = [1,2,3,4,5,6,7,8,9]
+
+    const circleColors = d3.scaleOrdinal().domain(circleData).range(d3.schemeSet3);
+    const lineColors = d3.scaleOrdinal().domain(lineData).range(d3.schemeSet3);
+
+    const circleNodes = svg
+        .selectAll('circle')
+        .data(testNodes)
+        .enter()
+        .append('circle')
+        .attr('r', 12)
+        .attr('fill', function(d){return circleColors(d) });
+
+    const textNodes = svg
+        .selectAll('text')
+        .data(testNodes)
+        .enter()
+        .append('text')
+        .text(d => d.id);
+
+    const lineLinks = svg
+        .selectAll('line')
+        .data(testLinks)
+        .enter()
+        .append('line')
+        .attr('stroke', function(d){return lineColors(d) });
+
+    testSim.on('tick', () => {
+        circleNodes
+            .attr('cx', d => d.x)
+            .attr('cy', d => d.y)
+        textNodes
+            .attr('x', d => d.x)
+            .attr('y', d => d.y)
+        lineLinks
+            .attr('x1', d => d.source.x)
+            .attr('y1', d => d.source.y)
+            .attr('x2', d => d.target.x)
+            .attr('y2', d => d.target.y)
+        console.log('tick');
+    });
+
+}(d3));
