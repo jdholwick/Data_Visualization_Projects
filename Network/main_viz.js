0d1b0d4f21127c4b20ef4ed7cbb4c80488e932b2
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2021-11-15 17:51:53 -0700

    Major Changes:   * None
    Minor Changes:
      * Color of node now showing the "colour" the original author deemed appropriate.
      * The text is easier to read with the color changes now.
      * Edges are now dark red and a little thicker.

--- Network/main_viz.js
+++ Network/main_viz.js
@@ -11,24 +11,24 @@ import {testNodes, testLinks} from "./data/data.js";
 console.log(testDataPoint, testNodes, testLinks);
 
 const testSim = d3.forceSimulation(testNodes)
-    .force("charge", d3.forceManyBody().strength(-900))
-    .force("link", d3.forceLink(testLinks))
+    .force("charge", d3.forceManyBody().strength(-1500))
+    .force("link", d3.forceLink(testLinks))//.distance(10))
     .force("center", d3.forceCenter(centX, centY));
 
 // For the D3 'scaleOrdinal()' function it doesn't matter what the data is... it's just counting the number of elements.
-const circleData = [1,2,3]//,4,5,6,7,8,9,10,11,12,13,14,15,16]
-const lineData = [1,2,3]//,4,5,6,7,8,9,10,11,12,13,14,15]
+//const circleData = [1,2,3]//,4,5,6,7,8,9,10,11,12,13,14,15,16]
+//const lineData = [1,2,3]//,4,5,6,7,8,9,10,11,12,13,14,15]
 
-const circleColors = d3.scaleOrdinal().domain(circleData).range(d3.schemeSet3);
-const lineColors = d3.scaleOrdinal().domain(lineData).range(d3.schemeSet3);
+//const circleColors = d3.scaleOrdinal().domain(circleData).range(d3.schemeSet3);
+//const lineColors = d3.scaleOrdinal().domain(lineData).range(d3.schemeSet3);
 
 const circleNodes = svg
     .selectAll('circle')
     .data(testNodes)
     .enter()
     .append('circle')
-    .attr('r', (d) => d.value)//12)
-    .attr('fill', '#ee9b00');//function(d){return circleColors(d) });
+    .attr('r', (d) => (d.value)/1.2)//12)
+    .attr('fill', (d) => d.colour);//'#ee9b00');//function(d){return circleColors(d) });
 
 const textNodes = svg
     .selectAll('text')
@@ -37,6 +37,11 @@ const textNodes = svg
     .append('text')
     .attr('text-anchor', 'middle') // Puts text on middle of nodes
     .attr('alignment-baseline', 'middle')
+    .attr('fill', '#1d3557')
+    .attr('stroke-width', .65)
+    .attr('font-size', 27)
+    .attr('font-weight', 'bold')
+    .attr('stroke', 'white')
     .text(d => d.name);
 
 const lineLinks = svg
@@ -44,20 +49,24 @@ const lineLinks = svg
     .data(testLinks)
     .enter()
     .append('line')
-    .attr('stroke', '#0a9396');//function(d){return lineColors(d) });
+    .attr('stroke-width', 1.3)
+    .attr('stroke', '#ae2012');//function(d){return lineColors(d) });
 
 testSim.on('tick', () => {
     circleNodes
         .attr('cx', d => d.x)
         .attr('cy', d => d.y)
-    textNodes
-        .attr('x', d => d.x)
-        .attr('y', d => d.y)
+
     lineLinks
         .attr('x1', d => d.source.x)
         .attr('y1', d => d.source.y)
         .attr('x2', d => d.target.x)
         .attr('y2', d => d.target.y)
-    console.log('tick');
+
+    textNodes
+        .attr('x', d => d.x)
+        .attr('y', d => d.y)
+
+    //console.log('tick');
 });
 
